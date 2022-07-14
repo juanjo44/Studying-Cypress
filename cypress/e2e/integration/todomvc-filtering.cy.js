6f9ec575a4934e8bf7ba0004a12890f80aa3c277
Juan Betancourt Osorio <juan.betancourto@perficient.com>
2022-07-14 15:24:31 -0500

    I have already ended the mocka class

--- cypress/e2e/integration/todomvc-filtering.cy.js
+++ cypress/e2e/integration/todomvc-filtering.cy.js
@@ -6,11 +6,22 @@ describe('filtering', () => {
         cy.get('.new-todo').type('Learn Cypress{enter}')
         cy.get('.new-todo').type('Learn JavaScript{enter}')
         cy.get('.new-todo').type('Use Cypress{enter}')
-        cy.get('.todo-list li:nth-child(2) .toggle').click()
+        cy.get('.todo-list li:nth-child(2) .toggle').click() //Sirve para seleccionar solo al segundo elemento de la lista
 
     })
-    
+
     it('should filter "Active" todos', () =>{
         cy.contains('Active').click()
+        cy.get(".todo-list li").should('have.length', 2)
+    })
+
+    it('should filter "Completed" todos', () =>{
+        cy.contains('Completed').click()
+        cy.get(".todo-list li").should('have.length', 1)
+    })
+
+    it('should filter "All" todos', () =>{
+        cy.contains('All').click()
+        cy.get(".todo-list li").should('have.length', 3)
     })
 })
